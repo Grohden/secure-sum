080d087857907e00f10308cfe39e047190b186fd
Gabriel Rohden <gabriel_rohden@outlook.com>
2018-08-21 17:25:02 -0300

    Added cypher and AES encryption to exchange the messages after the diffie hellman key calculations (and also got a big random prime)

--- src/client.py
+++ src/client.py
@@ -1,6 +1,8 @@
 import socket
 import random
 
+from security.aes import AESCipher
+from Crypto.Cipher import AES
 from security.diffie_hellman import calculate_key
 from utils.constants import DATA_SIZE, ADDRESS, MAX_RANDOM, SHARED_BASE, SHARED_PRIME
 from utils.sockets import receive_message, send_msg
@@ -12,12 +14,32 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 def main():
     tcp_socket.connect(ADDRESS)
 
+    first = input("Type the first number: ")
+    second = input("Type the second number: ")
+
     client_diff_hellman = calculate_key(SHARED_BASE, SHARED_PRIME, secret_client_key)
     send_msg(tcp_socket, str(client_diff_hellman))
     a_value = receive_message(tcp_socket)
 
     shared_secret = calculate_key(a_value, SHARED_PRIME, secret_client_key)
-    print "Shared key:", shared_secret
+    print "The diffie hellman key is ", shared_secret, ", but don't tell anyone pls"
+
+    cipher = AESCipher(shared_secret)
+
+    # Creates the encrypted message
+    message = cipher.encrypt(str(first) + ":" + str(second))
+    print "The message to send is ", message
+
+    # Sends it to the server
+    send_msg(tcp_socket, message)
+
+    # Receives the message from the server
+    response = receive_message(tcp_socket)
+    print "The message received is ", response
+
+    # Decrypt the message
+    requested_sum = cipher.decrypt(response)
+    print "Your sum is: ", requested_sum
 
 
 main()
--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
 import socket
 import random
+import operator
+
 from security.diffie_hellman import calculate_key
+from security.aes import AESCipher
 from utils.constants import DATA_SIZE, ADDRESS, MAX_RANDOM, SHARED_BASE, SHARED_PRIME
 from utils.sockets import send_msg, receive_message
 
@@ -21,7 +24,26 @@ def main():
         b_value = receive_message(connection)
 
         shared_secret = calculate_key(b_value, SHARED_PRIME, secret_server_key)
-        print "Shared key:", shared_secret
+        print "The diffie hellman key is ", shared_secret, ", but don't tell anyone pls"
+
+        # Receive client numbers
+        encrypted = receive_message(connection)
+        print "The encrypted message received is ", encrypted
+
+        cipher = AESCipher(shared_secret)
+
+        # Decrypt and maps to a list of numbers
+        client_numbers = map(int, cipher.decrypt(encrypted).split(":"))
+
+        # Creates the message
+        message = str(reduce(operator.add, client_numbers))
+        print "The message to send is ", message
+
+        # Encrypts and sends the message
+        send_msg(connection, cipher.encrypt(message))
+
+        # Loooppppss brother
+        print "Waiting for client"
 
 
 main()
